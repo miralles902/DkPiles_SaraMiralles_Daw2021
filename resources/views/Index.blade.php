0748ed08f49315743aa1e24fb9ef9d86b25953d3
miralles_660 <miralles902@gmail.com>
2021-04-26 21:17:05 +0200

    Creado enlace de contacto de whatsapp y enlace a página de login

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Sara Miralles Roselló" />
-    <meta name="keywords" content="kitesurf, Piles, dkpiles, piles, playa, club, reunion"/>
-    <meta name="robots" content="index,follow"/>
+    <meta name="keywords" content="kitesurf, Piles, dkpiles, piles, playa, club, reunion" />
+    <meta name="robots" content="index,follow" />
 
     <title>DkPiles - Bienvenido</title>
 
@@ -33,18 +33,30 @@
         <span><i class="fas fa-sun"></i></span>
         <span><i class="fas fa-moon"></i></span>
     </button>
+    <!-- end boton modo oscuro -->
+
+    <!-- boton Whatsapp -->
+    <button class="whatsapp" id="whatsapp">
+    <a title="whatsapp" href="https://wa.me/+34622861169/?text=Me%20gustaría%20ponerme%20en%20contacto%20con%20vosotros" target="_blank" id="enlaceWhatsapp"><i class="fab fa-whatsapp"></i></a>
+    </button>
+    <!-- end boton Whatsapp -->
+
+    <!-- boton Login -->
+    <button class="login" id="login">
+    <a title="login" href="{{ url('/login') }}" target="_self" id="enlaceLogin"><i class="fas fa-key"></i></a>
+    </button>
+    <!-- end boton Login -->
 
     <!-- Menu -->
     <nav>
-    <a href="{{ url('/') }}">Página principal</a>
-    <a class="btn btn-primary" href="{{ url('/contacto') }}">Contacto</a>
-    <a class="btn btn-primary" href="{{ url('/tiempo') }}">El tiempo</a>
-    <a class="btn btn-primary" href="{{ url('/galeria') }}">Galeria</a>
-    <a class="btn btn-primary" href="{{ url('/login') }}">Login</a>
-    <a class="btn btn-primary" href="{{ url('/mercadillo') }}">Mercadillo</a>
-    <a class="btn btn-primary" href="{{ url('/noticias') }}">Noticias</a>
-    <a class="btn btn-primary" href="{{ url('/webcams') }}">Webcams</a>
-    <a class="btn btn-primary" href="{{ url('/val') }}">Valenciano</a>
+        <a href="{{ url('/') }}">Página principal</a>
+        <a class="btn btn-primary" href="{{ url('/contacto') }}">Contacto</a>
+        <a class="btn btn-primary" href="{{ url('/tiempo') }}">El tiempo</a>
+        <a class="btn btn-primary" href="{{ url('/galeria') }}">Galeria</a>
+        <a class="btn btn-primary" href="{{ url('/mercadillo') }}">Mercadillo</a>
+        <a class="btn btn-primary" href="{{ url('/noticias') }}">Noticias</a>
+        <a class="btn btn-primary" href="{{ url('/webcams') }}">Webcams</a>
+        <a class="btn btn-primary" href="{{ url('/val') }}">Valenciano</a>
     </nav>
     <!-- end menu -->
 
